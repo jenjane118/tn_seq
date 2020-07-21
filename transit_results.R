afec0349e62356a1b82e3713533abfb4b07a7e01
Jennifer J. Stiens <j.j.stiens@gmail.com>
2020-07-21 12:08:47 +0100

    update

---
+++ transit_results.R
@@ -0,0 +1,6 @@
+# looking at transit analysis results
+
+transit_data <- read.csv("bovis_TTR.txt", sep="\t", skip=11, header= FALSE)
+head(transit_data)
+colnames(transit_data)<-c("ORF","Name", "Descr", "no ins in ORF", "total TA in ORF", "len max run non-ins", "nt span for max run non-ins", "zbar", "call")
+head(transit_data)
