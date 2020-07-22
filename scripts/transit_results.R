258c76ec1dd3fda9506b277fc0cdf4bed5ba4ca8
Jennifer J. Stiens <j.j.stiens@gmail.com>
2020-07-22 15:58:47 +0100

    moved hmm files

--- scripts/transit_results.R
+++ scripts/transit_results.R
@@ -1,5 +1,7 @@
 # looking at transit analysis results
 
-transit_data <- read.csv("bovis_TTR.txt", sep="\t", skip=11, header= FALSE)
+transit_data <- read.csv("results/bovis_TTR.txt", sep="\t", skip=11, header= FALSE)
 head(transit_data)
 colnames(transit_data)<-c("ORF","Name", "Descr", "no ins in ORF", "total TA in ORF", "len max run non-ins", "nt span for max run non-ins", "zbar", "call")
+head(transit_data)
+
