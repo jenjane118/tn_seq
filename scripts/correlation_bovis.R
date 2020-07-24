1d9ece563f33674bd17e96e072528ce7da9c2963
Jennifer J. Stiens <j.j.stiens@gmail.com>
2020-07-24 16:20:17 +0100

    biotradis on mtb

--- scripts/correlation_bovis.R
+++ scripts/correlation_bovis.R
@@ -249,7 +249,7 @@ total_positions<-append(total_positions, D_sample)
 total_positons<-unique(total_positions)
 
 library(dplyr)
-data<-read.csv("results/bovis_fastqs.stats", header=T)
+data<-read.csv("results/new_bovis_fastqs.stats", header=T)
 bovis_stats <- select(data, 1, 7, 8)
 bovis_stats
 View(bovis_stats)
